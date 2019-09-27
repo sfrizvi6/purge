2b69ee4899c196dc3b3c112c591ff44658249744
Fatima Rizvi <fsyeda@linkedin.com>
2019-09-27 14:24:23 -0700

    hooking up to barcode lookup

--- app/src/main/java/com/example/purge/GetStartedFragment.java
+++ app/src/main/java/com/example/purge/GetStartedFragment.java
@@ -5,21 +5,29 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.purge.databinding.FragmentGetStartedBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -80,7 +88,7 @@ public class GetStartedFragment extends Fragment {
 
         // TODO: to obtain captured image
 //        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.qr_code);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.raisin_nut_bran_bar_code);
         FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
 
 //        initializeTextRecognition(image);
@@ -88,7 +96,7 @@ public class GetStartedFragment extends Fragment {
     }
 
     private void initializeTextRecognition(@NonNull FirebaseVisionImage image) {
-        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+        FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getCloudTextRecognizer();
         detector.processImage(image)
                 .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
                     @Override
@@ -106,14 +114,17 @@ public class GetStartedFragment extends Fragment {
     }
 
     private void initializeBarcodeScanning(@NonNull FirebaseVisionImage image) {
-        FirebaseVisionBarcodeDetectorOptions options = new FirebaseVisionBarcodeDetectorOptions.Builder().setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE, FirebaseVisionBarcode.FORMAT_AZTEC).build();
-        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance().getVisionBarcodeDetector();
         detector.detectInImage(image)
                 .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
                     @Override
-                    public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
-                        Log.i("Text read", firebaseVisionBarcodes.get(0).getDisplayValue());
-                        navigateToPreviewFragment(firebaseVisionBarcodes.get(0).getDisplayValue());
+                    public void onSuccess(@Nullable List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                        if (firebaseVisionBarcodes == null || firebaseVisionBarcodes.size() <= 0 || TextUtils.isEmpty(firebaseVisionBarcodes.get(0).getDisplayValue())) {
+                            navigateToPreviewFragment("Empty barcode received");
+                            return;
+                        }
+                        Log.i("Barcode read", firebaseVisionBarcodes.get(0).getDisplayValue());
+                        fetchProductInfo(firebaseVisionBarcodes.get(0).getDisplayValue());
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -125,12 +136,42 @@ public class GetStartedFragment extends Fragment {
     }
 
     private void navigateToPreviewFragment(String text) {
+        if (getActivity() == null || getActivity().getSupportFragmentManager() == null) {
+            return;
+        }
         FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
         PreviewFragment previewFragment = new PreviewFragment();
         Bundle bundle = new Bundle();
         bundle.putString("text", text);
         previewFragment.setArguments(bundle);
         fragmentTransaction.add(R.id.main_activity_container, previewFragment, PreviewFragment.TAG);
+        fragmentTransaction.addToBackStack(PreviewFragment.TAG);
         fragmentTransaction.commit();
+    }
+
+    private void fetchProductInfo(@NonNull String barcode) {
+        if (getContext() == null) {
+            return;
+        }
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url = "https://api.barcodelookup.com/v2/products?barcode=" + barcode + "&key=jvh8q0sjmt426omxfcsh9ptr6iug90";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        navigateToPreviewFragment("blue sky");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        navigateToPreviewFragment("barcode look up fail blooopers");
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        queue.add(jsonObjectRequest);
     }
 }
--- app/src/main/java/com/example/purge/PreviewFragment.java
+++ app/src/main/java/com/example/purge/PreviewFragment.java
@@ -15,7 +15,7 @@ import androidx.fragment.app.Fragment;
 
 public class PreviewFragment extends Fragment {
 
-    public static final String TAG = PreviewFragment.class.getSimpleName();
+    static final String TAG = PreviewFragment.class.getSimpleName();
 
     private FragmentPreviewBinding binding;
 
@@ -24,8 +24,7 @@ public class PreviewFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_preview, container, false);
         return binding.getRoot();
@@ -39,5 +38,12 @@ public class PreviewFragment extends Fragment {
             return;
         }
         binding.previewText.setText(arguments.getString("text"));
+
+        binding.closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
     }
 }
